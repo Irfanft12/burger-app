6e2730bb20e9c89fd43b0a5a0e45d8cbd382fc6c
Irfan Ahmad <irfanft12@gmail.com>
2020-07-22 15:06:53 +0530

    initial commit

---
+++ src/components/ui/modal/Modal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './Modal.module.css';
+import Aux from '../../../hoc/Auxiliary';
+import Backdrop from '../backdrop/Backdrop';
+
+const modal = props => (
+    <Aux>
+        <div 
+            className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100%)',
+                opacity: props.show ? '1' : '0'
+            }}
+        >
+            {props.children}
+        </div>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+    </Aux>
+);
+
+export default modal;
