ac5a4e58b0379195cd69a5d324c9964a46a651f9
Irfan Ahmad <irfanft12@gmail.com>
2020-07-23 14:19:56 +0530

    Menu updated

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
-import classes from './Layout.module.css'
+import classes from './Layout.module.css';
+import Toolbar from '../navigation/toolbar/Toolbar'
 
 const Layout = ( props ) => {
     return (
         <Aux>
-            <div>Toolbar, Side drawer, Backdrop</div>
+            <Toolbar />
             <main className={classes.Content}>
                 {props.children}
             </main>
